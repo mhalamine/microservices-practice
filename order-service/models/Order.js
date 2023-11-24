d57f9cb97f221c2efcecb8dc8f919a3503ae561a
mhalamine <mh.alamine@hotmail.com>
2023-11-24 14:59:10 +0200

    updated order service model and server

--- order-service/models/Order.js
+++ order-service/models/Order.js
@@ -1,9 +1,29 @@
 class Order {
-    constructor(id, products, totalPrice) {
-      this.id = id;
-      this.products = products;
-      this.totalPrice = totalPrice;
-    }
+  constructor(id, products, totalPrice) {
+    this.id = id;
+    this.products = products;
+    this.totalPrice = totalPrice;
   }
-  
-  module.exports = Order;
+
+  static getAll() {
+    return orders;
+  }
+
+  static getById(id) {
+    return orders.find((o) => o.id === id);
+  }
+
+  static create(orderData) {
+    const id = Date.now().toString();
+    const totalPrice = Order.calculateTotalPrice(orderData.products);
+    const newOrder = new Order(id, orderData.products, totalPrice);
+    orders.push(newOrder);
+    return newOrder;
+  }
+
+  static calculateTotalPrice(products) {
+    return products.reduce((total, product) => total + product.price, 0);
+  }
+}
+
+module.exports = Order;
--- order-service/server.js
+++ order-service/server.js
@@ -5,6 +5,14 @@ const orderRoutes = require('./routes/orderRoutes');
 const app = express();
 const PORT = 3002;
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(bodyParser.json());
 app.use('/api/orders', orderRoutes);
 
