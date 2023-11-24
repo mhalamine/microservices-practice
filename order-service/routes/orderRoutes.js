ac7978482d7cf4cbb50a4d695c6436c3ba0c71b8
mhalamine <mh.alamine@hotmail.com>
2023-11-24 14:53:12 +0200

    updated Order and product service routes

--- order-service/routes/orderRoutes.js
+++ order-service/routes/orderRoutes.js
@@ -2,6 +2,40 @@ const express = require('express');
 const router = express.Router();
 const Order = require('../models/Order');
 
-// Define routes here
+let orders = []; // In-memory storage for simplicity
+
+// Get all orders
+router.get('/', (req, res) => {
+  res.json(orders);
+});
+
+// Get a single order by ID
+router.get('/:id', (req, res) => {
+  const orderId = req.params.id;
+  const order = orders.find((o) => o.id === orderId);
+
+  if (!order) {
+    return res.status(404).json({ error: 'Order not found' });
+  }
+
+  res.json(order);
+});
+
+// Create a new order
+router.post('/', (req, res) => {
+  const { products } = req.body;
+  const id = Date.now().toString(); // For simplicity, use timestamp as ID
+  const totalPrice = calculateTotalPrice(products);
+
+  const newOrder = new Order(id, products, totalPrice);
+  orders.push(newOrder);
+
+  res.status(201).json(newOrder);
+});
+
+// Helper function to calculate total price of items in the order
+function calculateTotalPrice(products) {
+  return products.reduce((total, product) => total + product.price, 0);
+}
 
 module.exports = router;
--- product-service/routes/productRoutes.js
+++ product-service/routes/productRoutes.js
@@ -2,6 +2,59 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
-// Define routes here
+let products = []; // In-memory storage for simplicity
+
+// Get all products
+router.get('/', (req, res) => {
+  res.json(products);
+});
+
+// Get a single product by ID
+router.get('/:id', (req, res) => {
+  const productId = req.params.id;
+  const product = products.find((p) => p.id === productId);
+
+  if (!product) {
+    return res.status(404).json({ error: 'Product not found' });
+  }
+
+  res.json(product);
+});
+
+// Create a new product
+router.post('/', (req, res) => {
+  const { name, description, price, inventory } = req.body;
+  const id = Date.now().toString(); // For simplicity, use timestamp as ID
+
+  const newProduct = new Product(id, name, description, price, inventory);
+  products.push(newProduct);
+
+  res.status(201).json(newProduct);
+});
+
+// Update a product
+router.put('/:id', (req, res) => {
+  const productId = req.params.id;
+  const productIndex = products.findIndex((p) => p.id === productId);
+
+  if (productIndex === -1) {
+    return res.status(404).json({ error: 'Product not found' });
+  }
+
+  const { name, description, price, inventory } = req.body;
+  const updatedProduct = new Product(productId, name, description, price, inventory);
+
+  products[productIndex] = updatedProduct;
+
+  res.json(updatedProduct);
+});
+
+// Delete a product
+router.delete('/:id', (req, res) => {
+  const productId = req.params.id;
+  products = products.filter((p) => p.id !== productId);
+
+  res.json({ message: 'Product deleted successfully' });
+});
 
 module.exports = router;
