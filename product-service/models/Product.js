855dfed1c8076c2dd9a29d12c73f04f47d2599de
mhalamine <mh.alamine@hotmail.com>
2023-11-24 14:59:21 +0200

    updated product service model and server.js

--- product-service/models/Product.js
+++ product-service/models/Product.js
@@ -1,11 +1,39 @@
 class Product {
-    constructor(id, name, description, price, inventory) {
-      this.id = id;
-      this.name = name;
-      this.description = description;
-      this.price = price;
-      this.inventory = inventory;
-    }
+  constructor(id, name, description, price, inventory) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.inventory = inventory;
   }
-  
-  module.exports = Product;
+
+  static getAll() {
+    return products;
+  }
+
+  static getById(id) {
+    return products.find((p) => p.id === id);
+  }
+
+  static create(productData) {
+    const id = Date.now().toString();
+    const newProduct = new Product(id, ...productData);
+    products.push(newProduct);
+    return newProduct;
+  }
+
+  static update(id, productData) {
+    const index = products.findIndex((p) => p.id === id);
+    if (index !== -1) {
+      products[index] = new Product(id, ...productData);
+      return products[index];
+    }
+    return null;
+  }
+
+  static delete(id) {
+    products = products.filter((p) => p.id !== id);
+  }
+}
+
+module.exports = Product;
--- product-service/server.js
+++ product-service/server.js
@@ -5,6 +5,14 @@ const productRoutes = require('./routes/productRoutes');
 const app = express();
 const PORT = 3001;
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(bodyParser.json());
 app.use('/api/products', productRoutes);
 
